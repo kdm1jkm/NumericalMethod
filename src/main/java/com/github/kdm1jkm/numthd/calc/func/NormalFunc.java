434912216fe72ec5e316a86458b093ce76acc787
kdm <kdm1jkmm@gmail.com>
2021-05-08 19:16:55 +0900

    Change expression evaluation library

--- src/main/java/com/github/kdm1jkm/numthd/calc/func/NormalFunc.java
+++ src/main/java/com/github/kdm1jkm/numthd/calc/func/NormalFunc.java
@@ -1,37 +1,37 @@
 package com.github.kdm1jkm.numthd.calc.func;
 
-import com.fathzer.soft.javaluator.DoubleEvaluator;
-import com.fathzer.soft.javaluator.StaticVariableSet;
+import org.mariuszgromada.math.mxparser.Argument;
+import org.mariuszgromada.math.mxparser.Expression;
 
 import javax.swing.*;
 
 public class NormalFunc implements Func {
-    private final double[] x;
+    private final double[] xs;
     private final String expression;
 
     private double[] data = null;
 
-    public NormalFunc(double[] x, String expression) {
-        this.x = x;
+    public NormalFunc(double[] xs, String expression) {
+        this.xs = xs;
         this.expression = expression;
     }
 
     @Override
     public double[] calculate(JProgressBar progressBar) {
         progressBar.setMinimum(0);
-        progressBar.setMaximum(x.length);
+        progressBar.setMaximum(xs.length);
         if (data == null) {
-            data = new double[x.length];
-            DoubleEvaluator evaluator = new DoubleEvaluator();
-            StaticVariableSet<Double> variables = new StaticVariableSet<>();
+            data = new double[xs.length];
+            Argument x = new Argument("x");
+            Expression expression = new Expression(this.expression, x);
 
             for (int i = 0; i < data.length; i++) {
-                variables.set("x", x[i]);
+                x.setArgumentValue(xs[i]);
                 try {
-                    data[i] = evaluator.evaluate(expression, variables);
+                    data[i] = expression.calculate();
                 } catch (Exception e) {
                     data[i] = Double.NaN;
-                    System.out.printf("x = %g\n", x[i]);
+                    System.out.printf("x = %g\n", xs[i]);
                     System.out.println(e.getMessage());
                     System.out.println("---------------------------");
                 }
